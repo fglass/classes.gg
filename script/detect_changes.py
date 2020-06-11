9f16494c285944b32450654735e35ca3d5f5404f
Fred Glass <glasshcs@gmail.com>
2020-06-11 18:11:05 +0100

    feat: add logging to change detection script

--- script/detect_changes.py
+++ script/detect_changes.py
@@ -1,54 +1,54 @@
 import json
+import logging
 import requests
 
 DEFINITIONS_FILE = "../src/database.json"
+NIGHTBOT_CHANNEL_URL = "https://api.nightbot.tv/1/channels/t/"
+NIGHTBOT_COMMANDS_URL = "https://api.nightbot.tv/1/commands"
 
 
 def main():
-    with open(DEFINITIONS_FILE, 'r+') as file:
+    with open(DEFINITIONS_FILE, "r+") as file:
         database = json.load(file)
-        for entry in database.items():
-            query_nightbot(entry)
+        [query_nightbot(entry) for entry in database.items()]
+        logging.info(f"{len(database)} users checked")
 
 
 def query_nightbot(entry):
-    key, value = entry
-    url = f"https://api.nightbot.tv/1/channels/t/{key}"
-    print(f"Requesting {url}")
+    username, data = entry
+    url = NIGHTBOT_CHANNEL_URL + username
     response = requests.get(url).json()
 
-    if response.get('status') == 200:
-        user_id = response.get('channel', {}).get('_id')
-        commands = query_commands(user_id)
+    if response.get("status") == 200:
+        user_id = response.get("channel", {}).get("_id")
+        commands = {}
 
-        new_commands = {}
-        for cmd in commands:
-            new_commands[cmd['name']] = cmd['message']
+        for cmd in query_commands(user_id):
+            commands[cmd["name"]] = cmd["message"]
 
-        compare_commands(old=value.get("commands"), new=new_commands)
+        compare_commands(username, previous=data.get("commands", []), current=commands)
 
 
 def query_commands(user_id):
-    url = "https://api.nightbot.tv/1/commands"
     headers = {"nightbot-channel": user_id}
-    response = requests.get(url, headers=headers).json()
+    response = requests.get(NIGHTBOT_COMMANDS_URL, headers=headers).json()
 
-    if response.get('status') == 200:
-        return response.get("commands", {})
+    if response.get("status") == 200:
+        return response.get("commands", [])
 
-    return {}
+    return []
 
 
-def compare_commands(old, new):
-    for entry in old:
+def compare_commands(username, previous, current):
+    for entry in previous:
         cmd = entry["command"]
         msg = entry["message"]
-        print(f"Command {cmd}")
-        print(f"Old: {msg}")
-        print(f"New: {new.get(cmd)}")
-        print(f"Change: {msg != new.get(cmd)}")
-        print("-" * 10)
+        new_msg = current.get(cmd)
+
+        if msg != new_msg:
+            logging.info(f"[{username}] [{cmd}]: {new_msg}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     main()
