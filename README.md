966e4fd5c72e352963a7e4e3c5ea48db98201cfa
Fred Glass <glasshcs@gmail.com>
2020-06-21 13:44:20 +0100

    feat(frontend): add search field to app bar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## classes.gg
+
+Centralised repository of Call of Duty: Warzone loadouts
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,84 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from "@material-ui/core/Typography";
-import {makeStyles} from "@material-ui/core/styles";
+import { createStyles, fade, makeStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    root: {
+        flexGrow: 1,
+    },
     title: {
+        flexGrow: 1,
+        display: 'block',
         paddingTop: theme.spacing(0.5),
         paddingLeft: theme.spacing(1),
         fontFamily: 'Bebas Neue',
         fontSize: '1.52rem',
     },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: theme.spacing(1),
+        width: 'auto%',
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                 width: '20ch',
+            },
+        },
+    },
 }));
+
 
 export default function Header() {
     const classes = useStyles()
     return (
-        <AppBar position="relative">
-            <Toolbar>
-                <img src={"/images/logo.svg"} alt="Classes.gg" />
-                <Typography className={classes.title}>
-                    Classes.gg
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <img src={"/images/logo.svg"} alt="Logo" />
+                    <Typography className={classes.title}>
+                        Classes.gg
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
