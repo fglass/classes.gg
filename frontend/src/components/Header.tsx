5e2a13b3f6d51b2e816cb4442be3685a1865ff58
Fred Glass <glasshcs@gmail.com>
2020-06-21 12:45:10 +0100

    feat(frontend): split out text from logo image

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        paddingTop: theme.spacing(0.5),
+        paddingLeft: theme.spacing(1),
+        fontFamily: 'Bebas Neue',
+        fontSize: '1.52rem',
+    },
+}));
 
 export default function Header() {
+    const classes = useStyles()
     return (
         <AppBar position="relative">
             <Toolbar>
-                <img src={"/images/logo.png"} alt="Classes.gg" />
+                <img src={"/images/logo.svg"} alt="Classes.gg" />
+                <Typography className={classes.title}>
+                    Classes.gg
+                </Typography>
             </Toolbar>
         </AppBar>
     )
