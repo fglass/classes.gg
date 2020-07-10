3c830576133d74988543cb2d1123ec966380f242
Fred Glass <glasshcs@gmail.com>
2020-07-10 19:30:49 +0100

    feat(frontend): add affiliation link to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     footer: {
-        backgroundColor: theme.palette.background.paper,
         width: '100%',
         padding: theme.spacing(2.1),
+        backgroundColor: theme.palette.background.paper,
     },
+    affiliationLink: {
+        color: 'white',
+    }
 }));
 
 export default function Footer() {
     const classes = useStyles()
     const discordLink = "https://discord.com/invite/AdB3jey"
+    const affiliationLink = "https://www.reddit.com/r/MWLoadouts/"
+
     return (
         <footer className={classes.footer}>
             <Grid container spacing={1}>
@@ -27,10 +32,18 @@ export default function Footer() {
                 </Grid>
                 <Grid item>
                     <Typography variant="body2" color="textSecondary">
-                        {new Date().getFullYear()}{' © FG'}
+                        {new Date().getFullYear()}{" © FG"}
                     </Typography>
                 </Grid>
             </Grid>
+            <Box pl={0.35}>
+                <Typography variant="body2" color="textSecondary" >
+                    {"Affiliated with "}
+                    <a href={affiliationLink} className={classes.affiliationLink}>
+                        r/MWLoadouts
+                    </a>
+                </Typography>
+            </Box>
         </footer>
     )
 }
