56b35af26ee8067ebf863a1b603688a9879651f8
Fred Glass <glasshcs@gmail.com>
2021-06-03 23:02:29 +0100

    feat(db): add seven new players

--- backend/db/admin/add_player.py
+++ backend/db/admin/add_player.py
@@ -6,14 +6,14 @@ from model.player import Player
 
 
 NEW_PLAYER = Player(
-    username="fifakillvizualz",
-    avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/8b3404ed950b4fda-profile_image-300x300.jpeg",
+    username="ScummN",
+    avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/a1ab18a3-c807-4ad8-bdee-87d8b9a7be49-profile_image-300x300.png",
     command_source=CommandSource.NIGHTBOT.value,
     last_updated=datetime.now().isoformat(),
     loadouts={},
     spreadsheet={
-        # "id": "1SWU-40CNrKL1thqffc-877rr-BV5TXQfjyPVL3CzJ1I",
-        # "sheets": ["0"],
+        "id": "18KTt5UtHhY5nWqZt7GEMcUPM4UJjkoRqgamnbFAuFaM",
+        "sheets": ["0"],
     },
     views=0,
 )
